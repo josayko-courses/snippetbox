3fb493b60cc70b11a69947f38b52ccab02dc4eb6
Jonny Saykosy <josayko@pm.me>
2024-02-22 18:51:38 +0100

    41/426: HTML templating and inheritance

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -22,8 +21,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
 
+}
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
@@ -31,15 +30,4 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	w.Write([]byte("Create a new snippet..."))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
